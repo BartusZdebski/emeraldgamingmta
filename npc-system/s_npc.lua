7f4e1cd9e9e67c941908da24d30cebe72e362d2b
BartusZdebski <bartuszdebski@gmail.com>
2019-08-21 22:22:09 +0200

    cmd /editnpc 2

--- npc-system/s_npc.lua
+++ npc-system/s_npc.lua
@@ -144,9 +144,8 @@ end
 addCommandHandler("gotonpc", goToNPC)
 
 -- /editnpc - By Bartuszdebski (20/08/19) [Trial Admin]
-local function editNPC(thePlayer, commandName, targetID, arg2, ...)
+local function editNPC(thePlayer, commandName, targetID, operation, value)
 	if exports.global:isPlayerTrialAdmin(thePlayer) then
-		local arg = {...}
 		local legend = "type, name"
 
 		if not targetID then return outputChatBox("SYNTAX: /" .. commandName .. " [NPC ID] [".. legend .."]", thePlayer, 75, 230, 10) end
@@ -155,38 +154,36 @@ local function editNPC(thePlayer, commandName, targetID, arg2, ...)
 
 		if not thePed then return outputChatBox("ERROR: An NPC with that ID does not exist!", thePlayer, 255, 0, 0) end
 
-		arg2 = string.lower(tostring(arg2))
+		operation = string.lower(tostring(operation))
 
-		if (arg2 == "type") then
-			local _type = table.concat({...}, " ")
-			if _type == "" then
+		if (operation == "type") then
+			if not value then
 				return outputChatBox("SYNTAX: /" .. commandName .. " [NPC ID] type [New Type]", thePlayer, 75, 230, 10) end
 
-			blackhawk:setElementDataEx(thePed, "npc:type", tonumber(_type), true)
+			blackhawk:setElementDataEx(thePed, "npc:type", tonumber(value), true)
 
 			reloadNPC(tonumber(targetID))
 
-			outputChatBox("You have changed NPC #" .. targetID .. "'s type to '" .. tostring(_type) .. "'.", thePlayer, 0, 255, 0)
+			outputChatBox("You have changed NPC #" .. targetID .. "'s type to '" .. tostring(value) .. "'.", thePlayer, 0, 255, 0)
 
-		elseif(arg2 == "name") then
-			if table.concat({...}, " ") == "" then
+		elseif(operation == "name") then
+			if not value then
 				return outputChatBox("SYNTAX: /" .. commandName .. " [NPC ID] name [New Name]", thePlayer, 75, 230, 10) end
 
-			local name = table.concat({...}, " ")
-			if string.len(name) > 25 then
+			if string.len(value) > 25 then
 				outputChatBox("ERROR: The NPC name cannot be longer than 25 characters.", thePlayer, 255, 0, 0)
 				return false
 			end
 
-			blackhawk:setElementDataEx(thePed, "npc:name", tostring(name), true)
-			blackhawk:setElementDataEx(thePed, "name", tostring(name), true)
+			blackhawk:setElementDataEx(thePed, "npc:name", tostring(value), true)
+			blackhawk:setElementDataEx(thePed, "name", tostring(value), true)
 
 			reloadNPC(tonumber(targetID))
 
 			local thePlayerName = exports.global:getStaffTitle(thePlayer)
 
-			outputChatBox("You have changed NPC #" .. targetID .. "'s name to '" .. tostring(name) .. "'.", thePlayer, 0, 255, 0)
-			exports.global:sendMessageToManagers("[INFO] " .. thePlayerName .. " changed NPC #" .. targetID .. "'s name to " .. name)
+			outputChatBox("You have changed NPC #" .. targetID .. "'s name to '" .. tostring(value) .. "'.", thePlayer, 0, 255, 0)
+			exports.global:sendMessageToManagers("[INFO] " .. thePlayerName .. " changed NPC #" .. targetID .. "'s name to " .. value)
 		end
 	end
 end
